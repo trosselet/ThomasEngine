2979a1b201354d6b8631c1eed3cbb57077bf068e
Thomas <trosselet@gaming.tech>
2025-11-03 16:39:55 +0100

    Add Visual Studio solution and project files
    Introduced initial Visual Studio solution (ThomasEngine.sln) and project files for Engine, Gameplay, Render, and Tools modules. Updated .gitignore to allow IDE files. This sets up the project structure for building and managing the codebase in Visual Studio.

--- src/Render/pch.h
+++ src/Render/pch.h
@@ -14,6 +14,22 @@
 #pragma comment(lib, "dxguid.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
+
+#include <fbxsdk.h>
+
+#ifdef _DEBUG
+#pragma comment(lib, "libfbxsdk-mt.lib")
+#pragma comment(lib, "zlib-mt.lib")
+#pragma comment(lib, "libxml2-mt.lib")
+#else
+#pragma comment(lib, "libfbxsdk-mt.lib")
+#pragma comment(lib, "zlib-mt.lib")
+#pragma comment(lib, "libxml2-mt.lib")
+#endif
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "winmm.lib")
+
 #include "../Tools/Header/PrimitiveTypes.h"
 #include "../Tools/Header/Utils.h"
 #include "../Tools/Header/Color.h"
